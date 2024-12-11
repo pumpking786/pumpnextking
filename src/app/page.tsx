bcbaa3e6aa5d81e7349fdff3dda309cc06bcf90e
Pramit Amatya <pramitamatya786@gmail.com>
2024-12-12 01:09:54 +0545

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import HeroSection from "@/components/HeroSection";
+import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-12">
-      <HeroSection />
+    <main className="flex min-h-screen flex-col bg-[#121212] ">
+      <Navbar />
+      <div className="containe mt-24 mx-auto px-12 py-12">
+        <HeroSection />
+      </div>
     </main>
   );
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,36 +1,58 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+// import { motion } from "framer-motion";
+import Link from "next/link";
 const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            Hello, I'm Pramit
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 ">
+              Hello, I'm{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              className="text-3xl sm:text-4xl lg:text-5xl"
+              sequence={[
+                "Pramit",
+                1000,
+                "Web Developer",
+                1000,
+                "Chess Coach",
+                1000,
+                "PumpKing",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
           </h1>
-          <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio saepe
-            dolorum dicta reiciendis quas esse magnam non, labore ad quidem
-            dolores obcaecati optio voluptas nemo, sunt ipsa qui exercitationem
-            suscipit!
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Thriving to become the best, driven by a passion for continuous
+            learning, and committed to making the world a more beautiful and
+            better place.
           </p>
           <div>
-            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
               Hire me
             </button>
-            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
               Download CV
             </button>
           </div>
         </div>
-        <div className="col-span-5 place-self-center mt-4 lg:mt-3 lg:ml-1">
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative mt-2">
+        <div className="col-span-5 place-self-center mt-9 lg:mt-3 lg:ml-8 ml-3">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[350px] relative">
             <Image
               src="/images/ok.jpg"
               alt="Profile"
-              className="rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+              className="rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mt-2"
               width={400} // Match the parent container's width for large screens
-              height={400} // Match the parent container's height for large screens
+              height={350} // Match the parent container's height for large screens
               objectFit="cover" // Ensures the image covers the circular area without distortion
             />
           </div>
